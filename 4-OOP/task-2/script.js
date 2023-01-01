76eaf2684cf51cfa1e6e95c824b56ebf6e07bc7b
AmetJunior <ametsejdaliev@gmail.com>
2023-01-01 18:23:24 +0300

    01.01.2023-inheritance-13.2

--- 4-OOP/task-2/script.js
+++ 4-OOP/task-2/script.js
@@ -3,20 +3,20 @@ class Person {
         this.name = name;
         this.age = age;
     }
-    compareAge(person1, person2, person3) {
-       
-        const InPerson1 = Object.values(person1)
-        const InPerson2 = Object.values(person2)
-        const InPerson3 = Object.values(person3)
-
-        console.log(InPerson1, InPerson2, InPerson3)
-        if(InPerson1 > InPerson2 && InPerson1 >InPerson3) {
-            console.log()
+    compareAge(person) {
+        if (person.age > this.age) {
+            console.log(`${this.name} младше, чем ${person.name}`)
+        } else {
+            console.log(`${this.name} старше, чем ${person.name}`)
         }
-      
+
     }
 }
-const person1 = new Person('Jolin', 34);
-const person2 = new Person('Jotaro', 35);
-const person3 = new Person('Joseph', 36);
-person1.compareAge(person1, person2, person3);
+const person1 = new Person('Максим', 24);
+const person2 = new Person('Светлана', 36);
+const person3 = new Person('Ирина', 23);
+
+person1.compareAge(person2); // Максим младше, чем Светлана
+person2.compareAge(person3); // Светлана старше, чем Ирина
+person3.compareAge(person1); // Ирина младше, чем Максим
+
