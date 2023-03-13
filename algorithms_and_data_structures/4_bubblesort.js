6a3c0d89035df1328b1d48dfe12b056158cb3a91
AmetJunior <ametsejdaliev@gmail.com>
2023-03-13 16:59:27 +0300

    my-version-lol

--- algorithms_and_data_structures/4_bubblesort.js
+++ algorithms_and_data_structures/4_bubblesort.js
@@ -1,13 +1,14 @@
-const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,]
+//mymethods+selection+bubblesort
+onst arr = [1,2,3,4,5,6,7,8,9]
 let count = 0
 
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if (array[j + 1] < array[j]) {
-                let tmp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = tmp
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[i]) {
+                let tmp = array[i]
+                array[i] = array[j]
+                array[j] = tmp
             }
             count+=1
         }
