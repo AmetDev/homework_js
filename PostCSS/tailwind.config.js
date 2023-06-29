a835b7e4931fec3b9b6b94189313d25008456e6a
AmetJunior <ametsejdaliev@gmail.com>
2023-06-29 23:03:55 +0300

    necessary-to-learn-customizing

--- PostCSS/tailwind.config.js
+++ PostCSS/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./dist/index.html", "./src/**/*.{html,js}"],
-  darkMode: 'class',
+  
   theme: {
     extend: {},
   },
